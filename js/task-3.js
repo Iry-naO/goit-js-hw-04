eb778405ea92a3dbd7fe916388f8e7a99e5cec31
Iryna_Oksamytna <ira.strynadko@gmail.com>
2025-12-13 21:34:44 +0200

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -20,6 +20,23 @@
 // Метод getInfo() має повертати рядок формату < Username > has < amount > active hours!,
 // де < Username > — це ім'я профілю, а < amount > — кількість ігрових годин.
 
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
 profile.changeUsername('Marco');
